55c3187ded7fe9f1c2a2a15de176e4aac6708dc7
elijah.brunsvold@gmail.com <elijah.brunsvold@gmail.com>
2017-12-05 16:51:05 -0600

    first commit from command line

---
+++ hello.py
@@ -0,0 +1,20 @@
+def getName():
+    print('Hello World')
+    print('What is your name')
+    myName = input()
+    print('Your name is: ' + myName)
+    #print('The length of your name is: ' + str(len(myName)))
+
+    if(len(myName) > 5):
+        print('Your name is longer than 5 characters!')
+    else:
+        print('Your name is less that 5 characters!')
+      
+    print('How old are you? ')
+    age = input()
+    if(int(age) >=21):
+        print('You are old enough to drink!')
+    else:
+        print('You may not drink legally!')
+        
+getName()
